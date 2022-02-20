b4ed9dc7ba4f564aeb0a14835e6f017326ce0826
takemoto222 <omochi9797@icloud.com>
2022-02-20 16:17:36 +0900

    02_20

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+use App\Http\Controllers\RestController;
 
-Route::get('/hello', function () {
-    return response()->json([
-        'message' => 'Hello'
-    ], 200);
-});
+Route::apiResource('/v1/rest', RestController::class);
--- tests/Feature/RestControllerTest.php
+++ tests/Feature/RestControllerTest.php
@@ -5,18 +5,24 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Rest;
 
 class RestControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_index_rest()
     {
-        $response = $this->get('/');
-
+        $item = Rest::factory()->create();
+        $response = $this->get('/api/v1/rest');
         $response->assertStatus(200);
+        $response->assertJsonFragment([
+            'message' => $item->message,
+            'url' => $item->url
+        ]);
     }
 }
