f1b9214d11e236d57bea4034e84cf16fa64535fa
takemoto222 <omochi9797@icloud.com>
2022-02-20 14:34:14 +0900

    first

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/hello', function () {
+    return response()->json([
+        'message' => 'Hello'
+    ], 200);
+});
